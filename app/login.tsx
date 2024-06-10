d4db3bfc10f03b61c3a221fd92eb6e57db49e770
KhoaTranCoder <115625861+KhoaTranProDev@users.noreply.github.com>
2024-06-10 08:50:38 +0700

    Khoa
    update Login, SignUp Screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,14 @@
 import { Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Stack } from 'expo-router'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Login = () => {
+    const [showPassword, setshowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setshowPassword(!showPassword);
+    }
   return (
     <View style={styles.container}>
       <Stack.Screen options={{
@@ -11,7 +17,23 @@ const Login = () => {
       <Text style={styles.title}>Login</Text>
       <View style={styles.box}>
         <TextInput style={styles.textInput} placeholder='Email'/>
-        <TextInput style={styles.textInput} placeholder='Password'/>
+        <View style={styles.containerPassword}> 
+                <TextInput 
+                    // Set secureTextEntry prop to hide  
+                    //password when showPassword is false 
+                    secureTextEntry={!showPassword} 
+                    style={styles.input} 
+                    placeholder="Enter Password"
+                    placeholderTextColor="#aaa"
+                /> 
+                <MaterialCommunityIcons 
+                    name={showPassword ? 'eye-off' : 'eye'} 
+                    size={24} 
+                    color="#aaa"
+                    style={styles.icon} 
+                    onPress={toggleShowPassword} 
+                /> 
+            </View>
         <Link href={'home'} asChild>
             <TouchableOpacity style={styles.ContinueButton}>
                 <Text style={{
@@ -56,7 +78,7 @@ const Login = () => {
         <View style={styles.bottomItem}>
             <Text style={{
                 fontSize: 15,
-                fontWeight: "400",
+                fontWeight: "300",
                 color: "#fff"
             }}>Don't have an account?</Text>
             <Link href={'signup'} asChild>
@@ -83,8 +105,8 @@ const styles = StyleSheet.create({
     title:{
         marginTop: 200,
         marginHorizontal: 20,
-        fontSize: 32,
-        fontWeight: "700"
+        fontSize: 35,
+        fontWeight: "900"
     },
     box:{
         marginHorizontal: 15,
@@ -98,9 +120,31 @@ const styles = StyleSheet.create({
         marginHorizontal: 15,
         marginTop: 25,
         backgroundColor: '#fff',
+        color:"#333",
+        fontSize: 16,
         borderWidth: 0.5,
         borderRadius: 10,
-        padding: 15,
+        padding: 14,
+    },
+    containerPassword: { 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        height: 50, 
+        backgroundColor: '#f3f3f3', 
+        borderRadius: 10,
+        borderWidth: 0.5, 
+        padding: 14, 
+        marginHorizontal: 15,
+        marginTop: 15,
+    }, 
+    input: { 
+        flex: 1, 
+        color: '#333', 
+        fontSize: 16, 
+    }, 
+    icon: { 
+        marginLeft: 10, 
     },
     ContinueButton:{
         height: 50,
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,8 +1,16 @@
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Stack } from 'expo-router'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const SignUp = () => {
+
+    const [showPassword, setshowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setshowPassword(!showPassword);
+    }
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{
@@ -11,65 +19,50 @@ const SignUp = () => {
       <Text style={styles.title}>Sign Up</Text>
       <View style={styles.box}>
         <View style={styles.headerContent}>
-          <Text>Looks like you don't have an account.</Text>
-          <Text>Let's create a new account for you</Text>
+          <Text style={styles.titleContent}>Looks like you don't have an account.</Text>
+          <Text style={styles.titleContent}>Let's create a new account for you.</Text>
         </View>
+        <TextInput style={styles.textInput} placeholder='Name'/>
         <TextInput style={styles.textInput} placeholder='Email'/>
-        <TextInput style={styles.textInput} placeholder='Password'/>
-        <Link href={'home'} asChild>
-            <TouchableOpacity style={styles.ContinueButton}>
+        <View style={styles.containerPassword}> 
+                <TextInput 
+                    // Set secureTextEntry prop to hide  
+                    //password when showPassword is false 
+                    secureTextEntry={!showPassword} 
+                    style={styles.input} 
+                    placeholder="Enter Password"
+                    placeholderTextColor="#aaa"
+                /> 
+                <MaterialCommunityIcons 
+                    name={showPassword ? 'eye-off' : 'eye'} 
+                    size={24} 
+                    color="#aaa"
+                    style={styles.icon} 
+                    onPress={toggleShowPassword} 
+                /> 
+            </View>
+        <Text style={styles.textContent}>By selecting Create Account below, i agree to</Text>
+        <Text style={styles.textBoldContent}>Terms of Service & Privacy Policy</Text>
+        <TouchableOpacity style={styles.CreateButton}>
                 <Text style={{
                     fontSize: 18,
                     fontWeight: '600',
                     color: '#000'
-                }}>Continue</Text>
+                }}>Create Account</Text>
             </TouchableOpacity>
-        </Link>
-        <TouchableOpacity>
-            <Text style={{
-            fontSize: 15,
-            fontWeight: '400',
-            alignSelf: 'center',
-            marginTop: 10,
-            color: '#fff'
-        }}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <View style={styles.horizontalLine}></View>
-        <TouchableOpacity style={styles.fbButton}>
-            <Image 
-            style={{width: 30, height: 30, margin: 10}}
-            source={require('./(tabs)/img/facebook.png')}/>
-            <Text style={{
-                alignSelf: 'center',
-                fontSize: 16,
-                fontWeight: "600",
-                marginLeft: 50,
-            }}>Login with Facebook</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.ggButton}>
-            <Image 
-            style={{width: 30, height: 30, margin: 10}}
-            source={require('./(tabs)/img/google.png')}/>
-            <Text style={{
-                alignSelf: 'center',
-                fontSize: 16,
-                fontWeight: "600",
-                marginLeft: 50,
-            }}>Login with Google</Text>
-        </TouchableOpacity>
         <View style={styles.bottomItem}>
             <Text style={{
                 fontSize: 15,
-                fontWeight: "400",
+                fontWeight: "300",
                 color: "#fff"
-            }}>Don't have an account?</Text>
-            <Link href={'signup'} asChild>
+            }}>Already have an account?</Text>
+            <Link href={'login'} asChild>
                 <TouchableOpacity>
                     <Text style={{
                         fontSize: 16,
                         fontWeight: "700",
                         color: "#FFC300"
-                    }}>Sign Up</Text>
+                    }}>Login</Text>
                 </TouchableOpacity>
             </Link>
         </View>
@@ -81,18 +74,56 @@ const SignUp = () => {
 export default SignUp
 
 const styles = StyleSheet.create({
+    containerPassword: { 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        height: 50, 
+        backgroundColor: '#f3f3f3', 
+        borderRadius: 10,
+        borderWidth: 0.5, 
+        padding: 14, 
+        marginHorizontal: 15,
+        marginTop: 15,
+    }, 
+    input: { 
+        flex: 1, 
+        color: '#333', 
+        fontSize: 16, 
+    }, 
+    icon: { 
+        marginLeft: 10, 
+    },
   container:{
     flex: 1,
   },
   title:{
     marginTop: 200,
     marginHorizontal: 20,
-    fontSize: 32,
-    fontWeight: "700"
+    fontSize: 35,
+    fontWeight: "900"
 },
 headerContent:{
   marginHorizontal: 15,
   marginTop: 20
+},
+titleContent:{
+    fontSize: 16,
+    color:"#fff",
+    fontWeight: "300",
+},
+textContent:{
+    fontSize: 16,
+    color:"#fff",
+    fontWeight: "300",
+    marginHorizontal: 20,
+    marginTop: 10
+},
+textBoldContent:{
+    fontSize: 16,
+    color:"#FFC300",
+    fontWeight: "600",
+    marginHorizontal: 20,
 },
 box:{
     marginHorizontal: 15,
@@ -106,23 +137,25 @@ textInput:{
     marginHorizontal: 15,
     marginTop: 25,
     backgroundColor: '#fff',
+    color:"#333",
+    fontSize: 16,
     borderWidth: 0.5,
     borderRadius: 10,
     padding: 15,
 },
-ContinueButton:{
+inputPass:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+},
+CreateButton:{
     height: 50,
     marginHorizontal: 15,
-    marginTop: 25,
+    marginTop: 15,
     backgroundColor: '#FFC300',
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center'
-},
-horizontalLine:{
-    marginTop: 25,
-    marginHorizontal: 15,
-    borderWidth: 0.5,
 },
 fbButton:{
     height: 50,
